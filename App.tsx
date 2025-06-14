665ccaacd0dd3eb05ad698d0ff3180c4605e822e
d03x <dadanhidyt@gmail.com>
2025-06-14 22:18:17 +0700

    update

--- App.tsx
+++ App.tsx
@@ -1,6 +1,7 @@
 import { store } from "@/store";
 import { Provider } from "react-redux";
 import Application from "./Application";
+import { StatusBar } from "react-native";
 export default function App() {
   return (
     <Provider store={store}>
--- src/screens/Home/Tabs/HomeTabScreen.tsx
+++ src/screens/Home/Tabs/HomeTabScreen.tsx
@@ -1,12 +1,19 @@
-import { memo, useCallback } from "react";
+import { memo } from "react";
 import type { AppScreenProps } from "@/navigation/AppNavigation/AppScreensTypes";
 import * as Layout from "@/components/Layouts";
 import AppHeader from "./components/Header";
-import { useAppTheme } from "@/contexts/AppThemeProvider";
+import Animated from "react-native-reanimated";
+import { PanResponder } from "react-native";
 const HomeTabScreen = ({ navigation }: AppScreenProps) => {
+  const a = PanResponder.create({
+    onPanResponderRelease: (e) => {
+      console.log(e);
+    },
+  });
   return (
     <Layout.ThemedView>
       <AppHeader navigation={navigation} />
+      <Animated.View></Animated.View>
     </Layout.ThemedView>
   );
 };
